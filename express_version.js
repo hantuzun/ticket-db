b394704e685868372e144ddf4739cf4c1ed624d4
Asha <ashaanoosheh@aol.com>
2014-10-13 13:53:34 +0800

    Added a file with a simple server in Express instead of standard ugly HTTP. Use that file instead of the others.

---
+++ express_version.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+//app.listen(3000);
+http.createServer(app).listen(80);
--- mysql-request.js
+++ mysql-request.js
@@ -1,17 +1,18 @@
 var http = require('http');
 
 console.log('Node has been started...');
-http.createServer(function(req, res) {
-    console.log('Receving request...');
-    var callback = function(err, result) {
-        res.writeHead(200, {
-            'Content-Type' : 'x-application/json'
-        });
-        console.log('json:', result);
-        res.end(result);
-    };
-    getSQL(callback);
-}).listen(3000);
+http.createServer(
+	function(req, res) {
+		console.log('Receving request...');
+		var callback = function(err, result) {
+			res.writeHead(200, {'Content-Type' : 'x-application/json'});
+			console.log('json:', result);
+			res.end(result);
+		};
+	parseJSON(
+    //getSQL(callback);
+	}
+).listen(3000); //port #
 
 function getSQL(callback) {
     var mysql = require('mysql'); 
@@ -22,23 +23,25 @@ function getSQL(callback) {
         password: 'masterpass',
         port: 3306,
         database: 'ticketdb'
-        });
-
+        }
+	);
     client.connect();
+	
     var query = 'SELECT * FROM artists';
-    client.query(query, function(err, results, fields) {
-        if (err)
-            return callback(err, null);
+    client.query(query, 
+		function(err, results, fields) {
+			if (err)
+				return callback(err, null);
 
-        console.log('query: ', query);
-        console.log('The query-result is: ');
-        console.log(results);
+			console.log('query: ', query);
+			console.log('The query-result is: ');
+			console.log(results);
 
-        // wrap result-set as json
-        var json = JSON.stringify(results);
-
-        // Nest the callback 
-        client.end();
-        callback(null, json);
-    });
+			// wrap result-set as json
+			var json = JSON.stringify(results);
+			// Nest the callback 
+			client.end();
+			callback(null, json);
+		}
+	);
 };
