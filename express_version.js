45bd7a8ddca1648202dad6f074ac53457450f1d9
Asha <ashaanoosheh@aol.com>
2014-10-18 21:48:26 +0800

    Request parsing and method calling added

--- express_version.js
+++ express_version.js
@@ -3,7 +3,55 @@ var http = require('http');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('hello world');
+	res.send('hello world');
 });
-//app.listen(3000);
+
+//REGISTRATION
+app.get('/register', function(req, res){
+	var p = req.params;
+	var reg = registerUser(p.email, p.username, p.passhash, p.firstname, p.lastname);
+	if (reg == true){
+		res.render('registration-completed');
+	}
+	else {
+		res.locals.reason = reg;
+		res.render('registration-failed')
+});
+
+//LOGIN
+app.get('/login', function(req, res){
+	var p = req.params;
+	var login = performLogin(p.email, p.passhash);
+	if (login == true){
+		res.render('registration-completed');
+	}
+	else {
+		res.locals.reason = login;
+		res.render('login-failed')
+});
+
+//SEARCH
+app.get('/search', function(req, res){
+	var p = req.params;
+	var login = searchDB(p.table, ...);
+	if (login == true){
+		res.render('login-completed');
+	}
+	else {
+		res.locals.reason = login;
+		res.render('login-failed')
+});
+
+/*********************/
+/*********************/
 http.createServer(app).listen(80);
+/*********************/
+/*********************/
+
+function registerUser(email, username, passhash, firstname, lastname) {
+	return true;
+}
+
+function performLogin(email, passhash) {
+	return true;
+}
