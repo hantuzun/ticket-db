fa93edb19587831893fc29b4cf139ebe28199f34
Asha <ashaanoosheh@aol.com>
2014-11-04 23:53:49 +0800

    Finished Admin panel

--- site/mdb.js
+++ site/mdb.js
@@ -34,7 +34,7 @@ function search(table, filters, callback) {
 	    JOIN events ON artist_loc.event_id = events.event_id \
 	    JOIN artists ON artist_loc.artist_name = artists.name";
 	    
-	var events_selection = "events.event_id, events.venue, events.date, events.tickets_left, events.price_per_ticket, artists.name";
+	var events_selection = "events.event_id, events.event_name, events.venue, events.date, events.tickets_left, events.price_per_ticket, artists.name";
 	var artists_selection = "artists.name, artists.info";
 
 	if (table === "events") {
@@ -106,18 +106,25 @@ function showProfile(email, callback) {
 }
 
 //FOR ADMINS
-function modifyTable(table, updateOrDelete, keyColumn, changeColumn, keyVal, newVal, callback) {
+function modifyTable(table, updateOrDelete, keyColumn, keyVal, changeColumn, newVal, callback) {
 	var sql;
-	if (! updateOrDelete) {
-		sql = "UPDATE ? SET ? = ? WHERE ? = ?";
+	if (updateOrDelete) {
+		sql = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
 		sql = mysql.format(sql, [table, changeColumn, newVal, keyColumn, keyVal]);
 	} else {
-		sql = "DELETE FROM ? WHERE ? = ?";
+		sql = "DELETE FROM ?? WHERE ?? = ?";
 		sql = mysql.format(sql, [table, keyColumn, keyVal]);
 	}
+
 	queryDB(sql, callback);
 }
 
+//ALSO FOR ADMINS
+function showAll(table, callback) {
+	var sql = "SELECT * FROM " + table;
+	//sql = mysql.format(sql, [table]);
+	queryDB(sql, callback);
+}
 
 //GENERIC DB QUERY FUNCTION
 function queryDB(requestStr, callback) {
@@ -159,4 +166,5 @@ module.exports.performLogin = performLogin;
 module.exports.search = search;
 module.exports.purchase = purchase;
 module.exports.modifyTable = modifyTable;
+module.exports.showAll = showAll;
 module.exports.showProfile = showProfile;
